e86172654cecc7b59bf547f7de1397b8594ec0ab
Joel Hendrix <jhendrix@microsoft.com>
2022-10-25 14:15:01 -0700

    add descriptions

--- internal/debug/debug.go
+++ internal/debug/debug.go
@@ -5,4 +5,7 @@ package debug
 
 // dummy functions used when debugging is not enabled
 
+// Log writes the formatted string to stderr.
+// Level indicates the verbosity of the messages to log.
+// The greater the value, the more verbose messages will be logged.
 func Log(_ int, _ string, _ ...interface{}) {}
--- internal/debug/debug_debug.go
+++ internal/debug/debug_debug.go
@@ -21,6 +21,9 @@ func init() {
 	debugLevel = level
 }
 
+// Log writes the formatted string to stderr.
+// Level indicates the verbosity of the messages to log.
+// The greater the value, the more verbose messages will be logged.
 func Log(level int, format string, v ...interface{}) {
 	if level <= debugLevel {
 		logger.Printf(format, v...)

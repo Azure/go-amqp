Azure/go-amqp

commit 653899b3afe74ff3e80eb0438ff130d2d46fcb0e

Don't discard incoming frames while closing a session (#223)
* Don't discard incoming frames while closing a session

Keep the session mux running until the acknowledging end performative
has been received.
Added fmt.Stringer implementation to PerformEnd.

* send end performative before exiting mux on error

* wait for end performative during client-side end due to error

* fix README

session.go
@@ -60,11 +60,13 @@ type Session struct {
 	linksByKey map[linkKey]*link // mapping of name+role link
 	handles    *bitmap.Bitmap    // allocated handles
 
-	// used for gracefully closing link
-	close     chan struct{}
+	// used for gracefully closing session
+	close     chan *Error
 	closeOnce sync.Once
-	done      chan struct{} // part of internal public surface area
-	doneErr   error         // contains the error state returned from Close(); DO NOT TOUCH outside of session.go until done has been closed!
+
+	// part of internal public surface area
+	done    chan struct{} // closed when the session has terminated
+	doneErr error         // contains the error state returned from Close(); DO NOT TOUCH outside of session.go until done has been closed!
 }
 
 func newSession(c *Conn, channel uint16, opts *SessionOptions) *Session {
@@ -79,7 +81,7 @@ func newSession(c *Conn, channel uint16, opts *SessionOptions) *Session {
 		handleMax:      math.MaxUint32,
 		linksMu:        sync.RWMutex{},
 		linksByKey:     make(map[linkKey]*link),
-		close:          make(chan struct{}),
+		close:          make(chan *Error, 1), // buffered so we can queue up an error from the mux
 		done:           make(chan struct{}),
 	}
 
@@ -167,11 +169,14 @@ func (s *Session) begin(ctx context.Context) error {
 // The session will continue to wait for the response until the Client is closed.
 func (s *Session) Close(ctx context.Context) error {
 	s.closeOnce.Do(func() { close(s.close) })
+
 	select {
 	case <-s.done:
+		// mux has exited
 	case <-ctx.Done():
 		return ctx.Err()
 	}
+
 	var sessionErr *SessionError
 	if errors.As(s.doneErr, &sessionErr) && sessionErr.RemoteErr == nil && sessionErr.inner == nil {
 		// an empty SessionError means the session was closed by the caller
@@ -264,6 +269,8 @@ func (s *Session) mux(remoteBegin *frames.PerformBegin) {
 		nextIncomingID       = remoteBegin.NextOutgoingID
 		remoteIncomingWindow = remoteBegin.IncomingWindow
 		remoteOutgoingWindow = remoteBegin.OutgoingWindow
+
+		clientClosed bool // indicates a client-side close
 	)
 
 	for {
@@ -276,6 +283,12 @@ func (s *Session) mux(remoteBegin *frames.PerformBegin) {
 			txTransfer = nil
 		}
 
+		closed := s.close
+		if clientClosed {
+			// swap out channel so it no longer triggers
+			closed = nil
+		}
+
 		select {
 		// conn has completed, exit
 		case <-s.conn.done:
@@ -283,28 +296,14 @@ func (s *Session) mux(remoteBegin *frames.PerformBegin) {
 			return
 
 		// session is being closed by user
-		case <-s.close:
-			_ = s.txFrame(&frames.PerformEnd{}, nil)
+		case err := <-closed:
+			clientClosed = true
+			fr := frames.PerformEnd{Error: err}
+			debug.Log(1, "TX(Session): %s", fr)
+			_ = s.txFrame(&fr, nil)
+			// TODO: per spec, after end has been sent, the session is no longer allowed to send frames
 
-			// wait for the ack that the session is closed.
-			// we can't exit the mux, which deletes the session,
-			// until we receive it.
-		EndLoop:
-			for {
-				select {
-				case fr := <-s.rx:
-					_, ok := fr.Body.(*frames.PerformEnd)
-					if ok {
-						break EndLoop
-					}
-				case <-s.conn.done:
-					s.doneErr = s.conn.doneErr
-					return
-				}
-			}
-			return
-
-		// incoming frame for link
+		// incoming frame
 		case fr := <-s.rx:
 			debug.Log(1, "RX(Session): %s", fr.Body)
 
@@ -356,14 +355,12 @@ func (s *Session) mux(remoteBegin *frames.PerformBegin) {
 					// This is a protocol error:
 					//       "[...] MUST be set if the peer has received
 					//        the begin frame for the session"
-					_ = s.txFrame(&frames.PerformEnd{
-						Error: &Error{
-							Condition:   ErrCondNotAllowed,
-							Description: "next-incoming-id not set after session established",
-						},
-					}, nil)
+					s.close <- &Error{
+						Condition:   ErrCondNotAllowed,
+						Description: "next-incoming-id not set after session established",
+					}
 					s.doneErr = errors.New("protocol error: received flow without next-incoming-id after session established")
-					return
+					continue
 				}
 
 				// "When the endpoint receives a flow frame from its peer,
@@ -418,8 +415,12 @@ func (s *Session) mux(remoteBegin *frames.PerformBegin) {
 				link, linkOk := s.linksByKey[linkKey{name: body.Name, role: !body.Role}]
 				s.linksMu.RUnlock()
 				if !linkOk {
+					s.close <- &Error{
+						Condition:   ErrCondNotAllowed,
+						Description: "received mismatched attach frame",
+					}
 					s.doneErr = fmt.Errorf("protocol error: received mismatched attach frame %+v", body)
-					return
+					continue
 				}
 
 				link.remoteHandle = body.Handle
@@ -488,10 +489,24 @@ func (s *Session) mux(remoteBegin *frames.PerformBegin) {
 				delete(deliveryIDByHandle, link.handle)
 
 			case *frames.PerformEnd:
-				_ = s.txFrame(&frames.PerformEnd{}, nil)
+				// there are two possibilities:
+				// - this is the ack to a client-side Close()
+				// - the peer is ending the session so we must ack
+
+				if clientClosed {
+					return
+				}
+
+				// TODO: per spec, when end is received, we're no longer allowed to receive frames
+
+				// peer detached us with an error, save it and send the ack
 				if body.Error != nil {
 					s.doneErr = body.Error
 				}
+
+				fr := frames.PerformEnd{}
+				debug.Log(1, "TX(Session): %s", fr)
+				_ = s.txFrame(&fr, nil)
 				return
 
 			default:

internal/frames/frames.go
@@ -1310,6 +1310,10 @@ type PerformEnd struct {
 
 func (e *PerformEnd) frameBody() {}
 
+func (d PerformEnd) String() string {
+	return fmt.Sprintf("End{Error: %v}", d.Error)
+}
+
 func (e *PerformEnd) Marshal(wr *buffer.Buffer) error {
 	return encoding.MarshalComposite(wr, encoding.TypeCodeEnd, []encoding.MarshalField{
 		{Value: e.Error, Omit: e.Error == nil},

commit b7b783d2bc57da9d48df13da726d10e73d788d59

Don't swallow unexpected frames
Terminate a session/link on receiving an unexpected frame.
The change uncovered a race introduced in #223 where it's possible to
send an *Error to a closed s.close causing a panic.

link.go
@@ -248,11 +248,9 @@ func (l *link) muxHandleFrame(fr frames.FrameBody, clientClosed bool) error {
 		return &LinkError{RemoteErr: fr.Error}
 
 	default:
-		// TODO: evaluate
 		debug.Log(1, "RX (link): unexpected frame: %s", fr)
+		return &LinkError{inner: fmt.Errorf("internal error: unexpected frame %T", fr)}
 	}
-
-	return nil
 }
 
 // Close closes the Sender and AMQP link.

session.go
@@ -164,7 +164,9 @@ func (s *Session) begin(ctx context.Context) error {
 func (s *Session) Close(ctx context.Context) error {
 	var ctxErr error
 	s.closeOnce.Do(func() {
-		close(s.close)
+		// we can't simply close(s.close) as that can race with
+		// the mux receiving an invalid frame during shutdown
+		s.close <- nil
 		select {
 		case <-s.done:
 			// mux has exited
@@ -298,6 +300,15 @@ func (s *Session) mux(remoteBegin *frames.PerformBegin) {
 			// swap out channel so it no longer triggers
 			closed = nil
 		}
+
+		// notes on client-side closing session
+		// when session is closed, we must keep the mux running until the ack'ing end performative
+		// has been received. during this window, the session is allowed to receive frames but cannot
+		// send them.
+		// client-side close happens either by user calling Session.Close() or due to mux initiated
+		// close due to a violation of some invariant (see sending &Error{} to s.close). in the case
+		// that both code paths have been triggered, we must be careful to preserve the error that
+		// triggered the mux initiated close so it can be surfaced to the caller.
 
 		select {
 		// conn has completed, exit
@@ -520,8 +531,6 @@ func (s *Session) mux(remoteBegin *frames.PerformBegin) {
 					return
 				}
 
-				// TODO: per spec, when end is received, we're no longer allowed to receive frames
-
 				// peer detached us with an error, save it and send the ack
 				if body.Error != nil {
 					s.doneErr = body.Error
@@ -529,11 +538,16 @@ func (s *Session) mux(remoteBegin *frames.PerformBegin) {
 
 				fr := frames.PerformEnd{}
 				_ = s.txFrame(&fr, nil)
+
+				// per spec, when end is received, we're no longer allowed to receive frames
 				return
 
 			default:
-				// TODO: evaluate
 				debug.Log(1, "RX (Session): unexpected frame: %s\n", body)
+				closeWithError(&Error{
+					Condition:   ErrCondInternalError,
+					Description: "received unexpected frame",
+				}, fmt.Errorf("internal error: unexpected frame %T", body))
 			}
 
 		case fr := <-txTransfer:

conn_test.go
@@ -776,7 +776,6 @@ func TestClientNewSessionInvalidInitialResponse(t *testing.T) {
 }
 
 func TestClientNewSessionInvalidSecondResponseSameChannel(t *testing.T) {
-	t.Skip("test hangs due to session mux eating unexpected frames")
 	firstChan := true
 	responder := func(req frames.FrameBody) ([]byte, error) {
 		switch req.(type) {
@@ -784,6 +783,8 @@ func TestClientNewSessionInvalidSecondResponseSameChannel(t *testing.T) {
 			return []byte{'A', 'M', 'Q', 'P', 0, 1, 0, 0}, nil
 		case *frames.PerformOpen:
 			return fake.PerformOpen("container")
+		case *frames.PerformClose:
+			return fake.PerformClose(nil)
 		case *frames.PerformBegin:
 			if firstChan {
 				firstChan = false
@@ -809,12 +810,13 @@ func TestClientNewSessionInvalidSecondResponseSameChannel(t *testing.T) {
 	cancel()
 	require.NoError(t, err)
 	require.NotNil(t, session)
-	// second session fails
+	// second session fails - times out as the ack is never received
 	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
 	session, err = client.NewSession(ctx, nil)
 	cancel()
 	require.Error(t, err)
 	require.Nil(t, session)
+	require.ErrorIs(t, err, context.DeadlineExceeded)
 	require.NoError(t, client.Close())
 }
 

sender_test.go
@@ -1059,3 +1059,39 @@ func TestNewSenderContextCancelled(t *testing.T) {
 	require.ErrorIs(t, err, context.Canceled)
 	require.Nil(t, snd)
 }
+
+func TestSenderUnexpectedFrame(t *testing.T) {
+	netConn := fake.NewNetConn(senderFrameHandlerNoUnhandled(SenderSettleModeUnsettled))
+
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	client, err := NewConn(ctx, netConn, nil)
+	cancel()
+	require.NoError(t, err)
+
+	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
+	session, err := client.NewSession(ctx, nil)
+	cancel()
+	require.NoError(t, err)
+
+	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
+	snd, err := session.NewSender(ctx, "target", nil)
+	cancel()
+	require.NoError(t, err)
+	require.NotNil(t, snd)
+
+	// senders don't receive transfer frames
+	fr, err := fake.PerformTransfer(0, 0, 1, []byte("boom"))
+	require.NoError(t, err)
+	netConn.SendFrame(fr)
+
+	// sender should now be dead
+	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
+	err = snd.Send(ctx, NewMessage([]byte("hello")), nil)
+	cancel()
+
+	var linkErr *LinkError
+	require.ErrorAs(t, err, &linkErr)
+	require.NotNil(t, linkErr.inner)
+	require.ErrorContains(t, err, "unexpected frame *frames.PerformTransfer")
+	require.NoError(t, client.Close())
+}

session_test.go
@@ -394,15 +394,19 @@ func TestSessionUnexpectedFrame(t *testing.T) {
 	cancel()
 	require.NoError(t, err)
 
-	// this frame is swallowed
+	// this frame causes the session to terminate
 	b, err := fake.EncodeFrame(frames.TypeSASL, 0, &frames.SASLMechanisms{})
 	require.NoError(t, err)
 	netConn.SendFrame(b)
 
-	ctx, cancel = context.WithTimeout(context.Background(), 100*time.Millisecond)
+	ctx, cancel = context.WithTimeout(context.Background(), 1*time.Second)
 	err = session.Close(ctx)
 	cancel()
-	require.NoError(t, err)
+	require.Error(t, err)
+	var sessionErr *SessionError
+	require.ErrorAs(t, err, &sessionErr)
+	require.NotNil(t, sessionErr.inner)
+	require.ErrorContains(t, err, "unexpected frame *frames.SASLMechanisms")
 	require.NoError(t, client.Close())
 }
 